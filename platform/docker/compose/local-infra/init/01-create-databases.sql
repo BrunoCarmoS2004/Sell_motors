e22639bccda4456e3840c8146888355f301ac8b5
BrunoCarmoS2004 <126525394+BrunoCarmoS2004@users.noreply.github.com>
2026-05-08 14:43:23 -0300

    Ajustes no lead com o docker

--- platform/docker/compose/local-infra/init/01-create-databases.sql
+++
@@ -1,7 +0,0 @@
-SELECT 'CREATE DATABASE sell_motors_vehicles'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'sell_motors_vehicles')
-\gexec
-
-SELECT 'CREATE DATABASE sell_motors_leads'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'sell_motors_leads')
-\gexec
