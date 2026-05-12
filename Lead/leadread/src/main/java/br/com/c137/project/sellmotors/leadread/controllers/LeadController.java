9be53289c5f896d8731924eddc61e7ab389f9e55
ViniciusItakura <viniciusitakura1@gmail.com>
2026-05-12 17:25:12 -0300

    Modulo de read

--- Lead/leadread/src/main/java/br/com/c137/project/sellmotors/leadread/controllers/LeadController.java
+++ Lead/leadread/src/main/java/br/com/c137/project/sellmotors/leadread/controllers/LeadController.java
@@ -1,6 +1,7 @@
 package br.com.c137.project.sellmotors.leadread.controllers;
 
 import br.com.c137.project.sellmotors.leadread.domains.leads.LeadDomain;
+import br.com.c137.project.sellmotors.leadread.dtos.gets.LeadFullDto;
 import br.com.c137.project.sellmotors.leadread.responses.ResponsePayload;
 import br.com.c137.project.sellmotors.leadread.services.LeadService;
 import br.com.c137.project.sellmotors.leadread.utils.MessageUtils;
@@ -30,18 +31,18 @@ public class LeadController {
     }
 
     @GetMapping()
-    ResponseEntity<PagedModel<LeadDomain>> listAllLeads(@PageableDefault Pageable pageable) {
+    ResponseEntity<PagedModel<LeadFullDto>> listAllLeads(@PageableDefault Pageable pageable) {
         return ResponseEntity.ok(leadService.findAllByTenantId(pageable));
     }
 
     @GetMapping("/name/{name}")
-    ResponseEntity<PagedModel<LeadDomain>> listAllLeadsByName(@PathVariable String name, @PageableDefault Pageable pageable) {
+    ResponseEntity<PagedModel<LeadFullDto>> listAllLeadsByName(@PathVariable String name, @PageableDefault Pageable pageable) {
         return ResponseEntity.ok(leadService.listByNameLikeIgnoreCase(name, pageable));
     }
 
     @GetMapping("/{id}")
-    ResponseEntity<ResponsePayload<LeadDomain>> findById(@PathVariable UUID id) {
-        LeadDomain lead = leadService.findById(id);
-        return ServiceUtils.createResponse(HttpStatus.OK, lead.getId(), lead, messageUtils.getMessage("leads.founded"));
+    ResponseEntity<ResponsePayload<LeadFullDto>> findById(@PathVariable UUID id) {
+        LeadFullDto lead = leadService.findById(id);
+        return ServiceUtils.createResponse(HttpStatus.OK, lead.id(), lead, messageUtils.getMessage("leads.founded"));
     }
 }
